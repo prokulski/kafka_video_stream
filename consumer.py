7cfeee6bfa7c589932a9fcf20aa1b052a11b10e1
Łukasz Prokulski <prokulski@gmail.com>
2020-05-23 12:46:29 +0200

    adding comments

--- consumer.py
+++ consumer.py
@@ -8,30 +8,31 @@ import matplotlib.pyplot as plt
 
 
 topic_name = 'testing'
+group_id = 'read_images'
+kafka_server = 'prokulski.science:9092'
+
 listner = KafkaConsumer(topic_name,
-	group_id='read_images',
-	bootstrap_servers='prokulski.science:9092')
+	group_id=group_id,
+	bootstrap_servers=kafka_server)
 
-
-i = 0
-
+# wait for message
 for msg in listner:
-	teraz = time.time()
-	i += 1
-	
+
+	# if message key is "image"?
 	if msg.key == b'image':
+		# deocde JSON to image
 		img = decode_image(msg.value)
+		# show image
 		cv2.imshow("Dest", img)
 	else:
+		# other key - just show message
 		ts = dt.fromtimestamp(int(msg.timestamp)/1000)
 		print(i, ts.strftime("%H:%M:%S @ %Y-%m-%d"))
 		print(f"Key = '{msg.key}'\nValue = '{msg.value}'\n")
 
-	print((time.time() - teraz)*1000)
-
+	# wait for ESC in preview window
 	if cv2.waitKey(1) == 27:
 		break
-
 
 
 cv2.destroyAllWindows()

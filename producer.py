482dc3ec629201d590c691c55b09854adca655d0
Łukasz Prokulski <prokulski@gmail.com>
2020-05-23 12:44:37 +0200

    adding comments

--- producer.py
+++ producer.py
@@ -5,35 +5,40 @@ import cv2
 from image_encoder import encode_image, decode_image
 
 
-# kafka producer
-producer = KafkaProducer(bootstrap_servers='prokulski.science:9092')
 topic_name = 'testing'
+kafka_server = 'prokulski.science:9092'
 
+# kafka producer
+producer = KafkaProducer(bootstrap_servers=kafka_server)
 
 # video capture
 cap = cv2.VideoCapture(0)
 
+
 while True:
-	teraz = time.time()
+	# grab image 
 	rst, img = cap.read()
 
 	if not rst:
 		break
 
+	# resize image
 	img = cv2.resize(img, (320,200))
 
+	# encode to JSON
 	data_json = encode_image(img)
 
+	# send to kafka
 	producer.send(topic_name, key=b'image', value=bytes(data_json, encoding='utf-8'))
 	producer.flush()
 
+	# show preview
 	cv2.imshow("Surce", img)
 
-	print((time.time() - teraz)*1000)
 
+	# exit on ESC
 	if cv2.waitKey(1) == 27:
 		break
-
 
 
 cv2.destroyAllWindows()
